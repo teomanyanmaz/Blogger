b2a1e06db8f34c83a4628e2270827530c67f700b
Teoman Yanmaz <teoman.yanmaz@hotmail.com>
2021-10-31 21:01:01 +0300

    master

--- selfgateway/router/usersService.js
+++ selfgateway/router/usersService.js
@@ -4,14 +4,14 @@ const api = require("./api");
 
 const apiUrl = api("http://localhost:4000");
 
-router.get("/users", async (req, res) => {
+router.get("/users", (req, res) => {
   apiUrl.get(req.path).then((resp) => {
     res.send(resp.data);
   });
 });
 
 router.post("/users", (req, res) => {
-  apiUrl.post(req.path,req.body).then((resp) => {
+  apiUrl.post(req.path, req.body).then((resp) => {
     res.send(resp.data);
   });
 });
