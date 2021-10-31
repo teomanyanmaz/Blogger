72fb5c4b30ff4de8cc6abf5444e6b7261117d91b
Teoman Yanmaz <teoman.yanmaz@hotmail.com>
2021-10-31 21:19:12 +0300

    master

--- selfgateway/index.js
+++ selfgateway/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
-const router = require("./router/router");
+const router = express.Router();
+const usersService = require("./router/usersService");
+const postsService = require("./router/postsService");
 
 app.use(express.json());
-
 app.use(router);
+
+router.use(usersService);
+router.use(postsService);
 
 app.listen(5000, () => {
   console.log("Listening on 5000");
--- selfgateway/router/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const usersService = require("./usersService");
-const postsService = require("./postsService");
-
-router.use(usersService);
-router.use(postsService);
-
-module.exports = router;
